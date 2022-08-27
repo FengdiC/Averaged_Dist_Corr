24f4d9a5287777341ecd671a960613bbcd2e47d8
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2022-08-27 11:54:54 -0600

    reacher

--- Components/hyperparam_tuning.py
+++ Components/hyperparam_tuning.py
@@ -10,22 +10,19 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 from train import train
 
-
 # param = {'batch_size':[100,200,500,1000],'buffer':[100,200,500,1000,3000],'lr':[0.0003],
 #          'LAMBDA_2':[10,40],'epoch':[1]}
 
 param = {'agent':['weighted_ppo','ppo'],'naive':[True, False]}
 
 args = utils.argsparser()
-args.env='Hopper-v2'
+# env, gamma, continuous are decided through args input
 args.batch_size = 128
-args.buffer = 4000
+args.buffer = 2048
 args.lr = 0.0003
 args.LAMBDA_2 = 10
-args.gamma=0.99
-args.continuous = True
 
-logger.configure(args.log_dir,['csv'], log_suffix='cartpole_ppo_naive-hyperparam-tune')
+logger.configure(args.log_dir,['csv'], log_suffix=str(args.env)+'-ppo-')
 
 for values in list(itertools.product(param['agent'],param['naive'])):
     args.agent = values[0]
