71cf806110ea89ee016574b041398503b98e2650
Gautham Vasan <gauthamv.529@gmail.com>
2022-10-09 03:26:25 -0600

    Variants of weighted ACKTR

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from Agents import batch_ac, weighted_batch_ac, ppo, weighted_ppo, acktr
+from Agents import batch_ac, weighted_batch_ac, ppo, weighted_ppo, acktr, weighted_acktr
 
 agents_dict = {"batch_ac":batch_ac.BatchActorCritic,
                "weighted_batch_ac":weighted_batch_ac.WeightedBatchActorCritic,
@@ -9,5 +9,6 @@ agents_dict = {"batch_ac":batch_ac.BatchActorCritic,
                'ppo_shared_gc':weighted_ppo.SharedWeightedPPO,
                'weighted_shared_ppo':weighted_ppo.WeightSeparateUpdatePPO,
                'acktr': acktr.ACKTR,
-               'weighted_acktr': acktr.WeightedACKTR,
+               'sw_acktr': acktr.SimpleWeightedACKTR,
+               'weighted_acktr': weighted_acktr.WeightedACKTR,
                }
