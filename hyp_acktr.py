5203006a607e18e3e0918a45ae7e3071d45dfe41
Gautham Vasan <gauthamv.529@gmail.com>
2022-10-10 02:57:12 -0400

    Smaller search window

--- hyp_acktr.py
+++ hyp_acktr.py
@@ -13,19 +13,14 @@ def grid_search_args(args):
     count = 0
     for batch_size in [32, 128, 512, 1024]:
         for lr in [0.3, 0.03, 0.003]:
-            for scale_weight in [1, 5, 10]:
+            for scale_weight in [1, 10]:
                 for value_loss_coef in [0.25, 1]:
-                    for entropy_coef in [0, 0.01]:
-                        for lr_weight in [0.003, 0.0003]:
                             count += 1
                             if count == args.hyp_seed:
                                 args.batch_size = batch_size
                                 args.lr = lr
                                 args.scale_weight = scale_weight
                                 args.value_loss_coef = value_loss_coef
-                                args.entropy_coef = entropy_coef
-                                args.lr_weight= lr_weight
-                                args.gamma = 0.99
                                 return args
     
     raise Exception("Hyp seed not found")
@@ -46,21 +41,20 @@ def main():
 
     args = utils.argsparser()
     args = grid_search_args(args)
-    
+    args.entropy_coef = 0.01
+    args.lr_weight= 0.0003
+    args.gamma = 0.99
+
     base_dir = '{}_{}_{}'.format(args.env, args.agent, args.naive)
     work_dir = os.path.join(currentdir, "results", base_dir, str(args.hyp_seed))
     make_dir(work_dir)        
 
-    seeds = range(30)
-
     if args.env in ['MountainCarContinuous-v0', 'Pendulum-v1', 'Reacher-v2']:
         args.continuous = True
-
-    for seed in seeds:
-        args.seed = seed
-        result = train(args)
-        savepath = os.path.join(work_dir, "returns_{}.txt".format(seed))
-        np.savetxt(savepath, result)
+        
+    result = train(args)
+    savepath = os.path.join(work_dir, "returns_{}.txt".format(seed))
+    np.savetxt(savepath, result)
 
 if __name__ == "__main__":
     main()
