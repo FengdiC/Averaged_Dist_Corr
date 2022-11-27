ff05513b3a42ff0f53c89a15a714f6d91d2f0800
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2022-11-26 21:08:31 -0700

    script

--- hopper_ppo_naive.sh
+++ hopper_ppo_naive.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=8000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=0-12:00
+#SBATCH --time=0-3:00
 #SBATCH --output=%N-%j-naive.out
 #SBATCH --account=rrg-ashique
 #SBATCH --array=1-200
@@ -15,4 +15,4 @@ SECONDS=0
 python spinningup/Hyperparam/naive_ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --env "Hopper-v4" --log_dir=$SCRATCH/avg_discount/logs/ &
 
 echo "Baseline job $seed took $SECONDS"
-sleep 12h
+sleep 3h
