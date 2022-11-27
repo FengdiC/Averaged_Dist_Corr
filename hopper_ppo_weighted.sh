f3b24047f3af324395c1fc509d8bd43378ef4b51
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2022-11-26 21:07:21 -0700

    script

--- hopper_ppo_weighted.sh
+++ hopper_ppo_weighted.sh
@@ -2,17 +2,17 @@
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=8000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
-#SBATCH --time=0-3:00
+#SBATCH --time=0-12:00
 #SBATCH --output=%N-%j.out
 #SBATCH --account=rrg-ashique
 #SBATCH --array=1-200
 
-module load mujoco/2.2.0
 source $HOME/Documents/ENV/bin/activate
-module load python/3.7
+module load python/3.10
+module load mujoco mpi4py
 
 SECONDS=0
 python spinningup/Hyperparam/weighted_ppo_tune.py --seed $SLURM_ARRAY_TASK_ID --env "Hopper-v4" --log_dir=$SCRATCH/avg_discount/logs/ &
 
 echo "Baseline job $seed took $SECONDS"
-sleep 3h
+sleep 12h
