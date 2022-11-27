a3c91b0aa606a9ee0e3332b1740bd5307634a996
Fengdi Che <fengdiche@Fengdis-MacBook-Pro.local>
2022-11-26 20:28:31 -0700

    script

--- hopper_ppo_naive.sh
+++ hopper_ppo_naive.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=4  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=8000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=0-3:00
-#SBATCH --output=%N-%j.out
+#SBATCH --output=%N-%j-naive.out
 #SBATCH --account=rrg-ashique
 #SBATCH --array=1-200
 
